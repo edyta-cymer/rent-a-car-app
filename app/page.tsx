ff0ad97f01423d63bbce07224a22828151b69cf4
Edyta Cymer <73744715+edyta-cymer@users.noreply.github.com>
2023-10-10 22:25:22 +0200

    Car Cataloge

---
+++ app/page.tsx
@@ -0,0 +1,24 @@
+// import Image from 'next/image'
+import Hero from '@/components/Hero'
+import SearchBar from '@/components/SearchBar'
+import CustomFilter from '@/components/CustomFilter'
+
+
+export default function Home() {
+  return (
+    <main className="overflow-hidden">
+      <Hero />
+      <div className='mt-12 padding-x padding-y max-width' id="discover">
+        <div className='home-text__container'><h1 className='text-4xl font-extrabold'>Car Cataloge</h1>
+          <p>Explore the cars you might like </p></div>
+        <div className='home__filters'>
+          <SearchBar />
+          <div className='home__filter-container'>
+            <CustomFilter title="fuel" />
+            <CustomFilter title="year" />
+          </div>
+        </div>
+      </div>
+    </main>
+  )
+}
---
+++ components/SearchManufacturer.tsx
@@ -0,0 +1,55 @@
+'use client'
+
+import { SearchManufacturerProps } from '@/types/index'
+import { Combobox, Transition } from '@headlessui/react'
+import Image from '@/node_modules/next/image'
+import { useState, Fragment } from 'react'
+import { manufacturers } from '@/constants/index'
+
+
+const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacturerProps) => {
+  const [query, setQuery] = useState('');
+
+  const filteredManufactures = query === '' ? manufacturers : manufacturers.filter((item) => item.toLowerCase().replace(/\s+/g, '').includes(query.toLowerCase().replace(/\s+/g, '')))
+  return (
+    <div className='search-manufacturer'>
+      <Combobox><div className='relative w-full'>
+
+        <Combobox.Button className="absolute top-[14px]">
+          <Image
+            src="./car-logo.svg"
+            alt="car logo"
+            width={20}
+            height={20}
+            className="m1-4" /></Combobox.Button>
+
+
+        <Combobox.Input
+          className="search-manufacturer__input"
+          placeholder="Volkswagen"
+          displayValue={(manufacturer: string) => manufacturer}
+          onChange={(e) => setQuery(e.target.value)} />
+
+
+        <Transition as={Fragment} leave="transition ease-in duration-100"
+          leaveFrom="opacity-100" leaveTo="opacity-0"
+          afterLeave={() => setQuery('')}>
+          <Combobox.Options>
+            {(filteredManufactures.map((item) => (
+
+              <Combobox.Option
+                key={item}
+                className={({ active }) => `relative search-manufacturer__option ${active ? 'bg-primary-blue text-white' : 'text -gray-900'}`}
+                value={item}>
+                {item}
+              </Combobox.Option>)))}
+
+          </Combobox.Options>
+        </Transition>
+      </div></Combobox>
+
+    </div>
+  )
+}
+
+export default SearchManufacturer
